edb67ec78f47603f5745fc494339eeb21c9f7d98
oubouhia <oubouhiamohamed@gmail.com>
2020-06-12 13:46:04 +0100

    r

--- woocomerce/wp-content/plugins/weglot/app/javascripts/metaboxes.js
+++
@@ -1,5 +0,0 @@
-import "./find-polyfill";
-import "./filter-polyfill";
-import InitUrlTranslate from "./metaboxes/url-translate";
-
-InitUrlTranslate();
--- woocomerce/wp-content/plugins/weglot/vendor/weglot/translation-definitions/mergeNodesList.js
+++
@@ -1,4 +0,0 @@
-// NOTE: This file should not contain any data itself.
-var mergeNodesList = require("./data/merge-nodes.json");
-mergeNodesList.v2.unshift("#text");
-exports.mergeNodesList = mergeNodesList;
